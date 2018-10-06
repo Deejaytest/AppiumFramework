953ab1374dac91e5e7266f274ccad86fc2c7f4d4
DJ <deepak1001jindal@gmail.com>
2018-10-06 21:43:27 +0530

    Commit2 from branch

--- src/gesture_Scroll.java
+++ src/gesture_Scroll.java
@@ -12,6 +12,9 @@ public class gesture_Scroll extends Baseclass {
 		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\"Popup Menu\").instance(0))");
 		//driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\"Views\").instance(0))");
 		System.out.println("branch test");
+		System.out.println("branch1 test");
+		System.out.println("branch2 test");
+
 
 
 		
